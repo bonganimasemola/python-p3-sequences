025cccb8749e8211b6ca1d078953b978da028e91
bonganimasemola <bonganimasemola@icloud.com>
2023-11-22 19:03:45 +0300

    sequences complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+
+    for i in range(length):
+        if i == 0:
+            fib_sequence.append(0)
+        elif i == 1:
+            fib_sequence.append(1)
+        else:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+
+    print(fib_sequence)
+
